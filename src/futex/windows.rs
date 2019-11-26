3688ca4caf15d6ab2cf389adc51af11010b2c472
Paul Dicker <pitdicker@gmail.com>
2019-11-26 15:15:30 +0100

    WIP

--- src/futex/windows.rs
+++ src/futex/windows.rs
@@ -13,33 +13,38 @@ use crate::windows::{Backend, BACKEND};
 
 impl Futex for AtomicI32 {
     fn wait(&self, compare: i32, timeout: Option<Duration>) -> WakeupReason {
-        if let Backend::Wait(f) = BACKEND.get() {
-            let address = self as *const _ as PVOID;
-            let compare_address = &compare as *const _ as PVOID;
-            let ms = convert_timeout_ms(timeout);
-            let r = (f.WaitOnAddress)(address, compare_address, mem::size_of::<AtomicI32>(), ms);
-            match r {
-                TRUE => WakeupReason::Unknown, // Can be any reason except TimedOut
-                FALSE | _ => match unsafe { GetLastError() } {
-                    ERROR_TIMEOUT if ms != INFINITE => WakeupReason::TimedOut,
-                    e => {
-                        debug_assert!(false, "Unexpected error of WaitOnAddress call: {}", e);
-                        WakeupReason::Unknown
-                    }
-                },
-            }
-        } else {
-            unreachable!();
+        let fn_ptr = WAIT_ON_ADDRESS.load(Ordering::Relaxed);
+        debug_assert!(fn_ptr != 0);
+        let WaitOnAddress: extern "system" fn(
+            Address: PVOID,
+            CompareAddress: PVOID,
+            AddressSize: SIZE_T,
+            dwMilliseconds: DWORD,
+        ) -> BOOL = unsafe { mem:transmute(fn_ptr) };
+
+        let address = self as *const _ as PVOID;
+        let compare_address = &compare as *const _ as PVOID;
+        let ms = convert_timeout_ms(timeout);
+        let r = WaitOnAddress(address, compare_address, mem::size_of::<AtomicI32>(), ms);
+        match r {
+            TRUE => WakeupReason::Unknown, // Can be any reason except TimedOut
+            FALSE | _ => match unsafe { GetLastError() } {
+                ERROR_TIMEOUT if ms != INFINITE => WakeupReason::TimedOut,
+                e => {
+                    debug_assert!(false, "Unexpected error of WaitOnAddress call: {}", e);
+                    WakeupReason::Unknown
+                }
+            },
         }
     }
 
     fn wake(&self) -> usize {
-        if let Backend::Wait(f) = BACKEND.get() {
-            (f.WakeByAddressAll)(self as *const _ as PVOID);
-            0 // `WakeByAddressAll` does not return the number of woken threads
-        } else {
-            unreachable!();
-        }
+        let fn_ptr = WAKE_BY_ADDRESS_ALL.load(Ordering::Relaxed);
+        debug_assert!(fn_ptr != 0);
+        let WakeByAddressAll: extern "system" fn(Address: PVOID) = unsafe { mem:transmute(fn_ptr) };
+
+        WakeByAddressAll(self as *const _ as PVOID);
+        0 // `WakeByAddressAll` does not return the number of woken threads
     }
 }
 
--- src/imp/windows.rs
+++ src/imp/windows.rs
@@ -17,7 +17,7 @@
 use core::cell::Cell;
 use core::mem;
 use core::ptr;
-use core::sync::atomic::Ordering::{Acquire, Relaxed, Release};
+use core::sync::atomic::Ordering::{self, Acquire, Relaxed, Release};
 use core::sync::atomic::{spin_loop_hint, AtomicI32, AtomicUsize};
 use core::time::Duration;
 
@@ -28,16 +28,37 @@ use winapi::shared::ntstatus::{STATUS_ALERTED, STATUS_SUCCESS, STATUS_TIMEOUT, S
 use winapi::um::libloaderapi::{GetModuleHandleA, GetProcAddress};
 use winapi::um::winnt::{ACCESS_MASK, BOOLEAN, EVENT_ALL_ACCESS, HANDLE, LPCSTR, PHANDLE, PVOID};
 
+// LARGE_INTEGER in WinAPI is a struct instead of integer, and not ergonomic to use.
+#[allow(non_camel_case_types)]
+type LARGE_INTEGER = i64;
+#[allow(non_camel_case_types)]
+type PLARGE_INTEGER = *mut LARGE_INTEGER;
+
 use crate::futex::{self, WakeupReason};
 use crate::RESERVED_MASK;
+
+pub(crate) const BACKEND_UNINITIALIZED: usize = 2;
+pub(crate) const BACKEND_WAIT_ON_ADDRESS: usize = 0;
+const BACKEND_NT_KEYED_EVENTS: usize = 1;
+
+pub(crate) static WINDOWS_BACKEND: usize = BACKEND_UNINITIALIZED;
+
+pub(crate) static WAIT_ON_ADDRESS: usize = 0;
+pub(crate) static WAKE_BY_ADDRESS_ALL: usize = 0;
+
+static NT_KEYED_EVENT_HANDLE: usize = 0;
+static NT_WAIT_FOR_KEYED_EVENT: usize = 0;
+static NT_RELEASE_KEYED_EVENT: usize = 0;
 
 //
 // Implementation of the Waiters trait
 //
 pub(crate) fn compare_and_wait(atomic: &AtomicUsize, compare: usize) {
-    match BACKEND.get() {
-        Backend::Wait(_) => futex::compare_and_wait(atomic, compare),
-        Backend::Keyed(_) => {
+    match WINDOWS_BACKEND.load(Ordering::Relaxed) {
+        BACKEND_WAIT_ON_ADDRESS => {
+            futex::compare_and_wait(atomic, compare)
+        }
+        BACKEND_NT_KEYED_EVENTS => {
             // Register the number of threads waiting. In theory we should be careful not to
             // overflow out of our counter bits. But it is impossible to have so many
             // threads waiting that it doesn't fit in 2^27 on 32-bit and 2^59 on 64-bit
@@ -66,19 +87,27 @@ pub(crate) fn compare_and_wait(atomic: &AtomicUsize, compare: usize) {
             }
             debug_assert!(atomic.load(Relaxed) & !RESERVED_MASK != compare);
         }
-        Backend::None => unreachable!(),
+        _ => {
+            init();
+            compare_and_wait(atomic, compare);
+        }
     }
 }
 
 pub(crate) fn store_and_wake(atomic: &AtomicUsize, new: usize) {
-    match BACKEND.get() {
-        Backend::Wait(_) => futex::store_and_wake(atomic, new),
-        Backend::Keyed(_) => {
+    match WINDOWS_BACKEND.load(Ordering::Relaxed) {
+        BACKEND_WAIT_ON_ADDRESS => {
+            futex::store_and_wake(atomic, new)
+        }
+        BACKEND_NT_KEYED_EVENTS => {
             let wake_count = atomic.swap(new, Release) & RESERVED_MASK;
             let key = atomic as *const AtomicUsize as PVOID;
             release_keyed_events(key, wake_count);
         }
-        Backend::None => unreachable!(),
+        _ => {
+            init();
+            store_and_wake(atomic, new);
+        }
     }
 }
 
@@ -92,93 +121,115 @@ const PARKED: i32 = 0x1;
 const NOTIFIED: i32 = 0x2;
 
 pub(crate) fn park(atomic: &AtomicI32, timeout: Option<Duration>) {
-    match BACKEND.get() {
-        Backend::Wait(_) => futex::park(atomic, timeout),
-        Backend::Keyed(_) => {
-            match atomic.compare_exchange(NOT_PARKED, PARKED, Release, Relaxed) {
-                Ok(_) => {}
-                Err(NOTIFIED) => {
-                    atomic.store(NOT_PARKED, Relaxed);
-                    return;
-                }
-                Err(_) => panic!(
-                    "Tried to call park on an atomic while \
-                     another thread is already parked on it"
-                ),
-            };
-            let key = atomic as *const AtomicI32 as PVOID;
-            loop {
-                let r = wait_for_keyed_event(key, timeout);
-                if timeout.is_some() {
-                    // We don't guarantee there are no spurious wakeups when there was a timeout
-                    // supplied.
-                    atomic.store(NOT_PARKED, Relaxed);
-                    break;
-                }
-                if let WakeupReason::Unknown = r {
-                    // The wakeup was not caused by an alert ot timeout, we know (almost) for sure
+    match atomic.compare_exchange(NOT_PARKED, PARKED, Release, Relaxed) {
+        Ok(_) => {}
+        Err(NOTIFIED) => {
+            atomic.store(NOT_PARKED, Relaxed);
+            return;
+        }
+        Err(_) => panic!(
+            "Tried to call park on an atomic while another thread is already parked on it"
+        ),
+    };
+    loop {
+        match WINDOWS_BACKEND.load(Ordering::Relaxed) {
+            BACKEND_WAIT_ON_ADDRESS => atomic.wait(PARKED, timeout),
+            BACKEND_NT_KEYED_EVENTS => {
+                let key = atomic as *const AtomicI32 as PVOID;
+                match wait_for_keyed_event(key, timeout) {
+                    WakeupReason::Unknown => {},
+                    _ if timeout.is_none() => {
+                    // FIXME: invert
+                    /// The wakeup was not caused by an alert ot timeout, we know (almost) for sure
                     // if the status is set to NOTIFIED. But this remains inherently racy, see
                     // the `compare_and_wait` implementation.
-                    if atomic
-                        .compare_exchange(NOTIFIED, NOT_PARKED, Relaxed, Relaxed)
-                        .is_ok()
-                    {
-                        break;
+                        continue;
                     }
                 }
             }
+            _ => {
+                init();
+                continue;
+            }
         }
-        Backend::None => unreachable!(),
+        if timeout.is_some() {
+            // We don't guarantee there are no spurious wakeups when there was a timeout
+            // supplied.
+            atomic.store(NOT_PARKED, Relaxed);
+            break;
+        }
+        if atomic.compare_exchange(NOTIFIED, NOT_PARKED, Relaxed, Relaxed).is_ok() {
+            break;
+        }
     }
 }
 
 pub(crate) fn unpark(atomic: &AtomicI32) {
-    match BACKEND.get() {
-        Backend::Wait(_) => futex::unpark(atomic),
-        Backend::Keyed(_) => {
+    match WINDOWS_BACKEND.load(Ordering::Relaxed) {
+        BACKEND_WAIT_ON_ADDRESS => {
+            futex::unpark(atomic)
+        }
+        BACKEND_NT_KEYED_EVENTS => {
             if atomic.swap(NOTIFIED, Release) == PARKED {
                 let key = atomic as *const AtomicI32 as PVOID;
                 release_keyed_events(key, 1);
             }
         }
-        Backend::None => unreachable!(),
+        BACKEND_UNINITALIZED | _ => {
+            init();
+            unpark(atomic);
+        }
     }
 }
 
 fn wait_for_keyed_event(key: PVOID, timeout: Option<Duration>) -> WakeupReason {
-    if let Backend::Keyed(f) = BACKEND.get() {
-        let nt_timeout = convert_timeout_100ns(timeout);
-        let timeout_ptr = nt_timeout
-            .map(|t_ref| t_ref as *mut _)
-            .unwrap_or(ptr::null_mut());
-        let r = (f.NtWaitForKeyedEvent)(f.handle, key, FALSE, timeout_ptr);
-        // `NtWaitForKeyedEvent` is an undocumented API where we don't known the possible
-        // return values, but they are most likely similar to `NtWaitForSingleObject`.
-        match r {
-            STATUS_SUCCESS => WakeupReason::Unknown,
-            STATUS_TIMEOUT if nt_timeout.is_some() => WakeupReason::TimedOut,
-            STATUS_ALERTED | STATUS_USER_APC => WakeupReason::Interrupt,
-            r => {
-                debug_assert!(
-                    false,
-                    "Unexpected return value of NtWaitForKeyedEvent: {}",
-                    r
-                );
-                WakeupReason::Unknown
-            }
+    let handle = NT_KEYED_EVENT_HANDLE.load(Ordering::Relaxed) as HANDLE;
+    debug_assert!(handle != ptr::null());
+    let fn_ptr = NT_WAIT_FOR_KEYED_EVENT.load(Ordering::Relaxed);
+    debug_assert!(fn_ptr != ptr::null());
+    let NtWaitForKeyedEvent: extern "system" fn(
+        EventHandle: HANDLE,
+        Key: PVOID,
+        Alertable: BOOLEAN,
+        Timeout: PLARGE_INTEGER,
+    ) -> NTSTATUS = unsafe { mem:transmute(fn_ptr) };
+
+    let nt_timeout = convert_timeout_100ns(timeout);
+    let timeout_ptr = nt_timeout
+        .map(|t_ref| t_ref as *mut _)
+        .unwrap_or(ptr::null_mut());
+    let r = NtWaitForKeyedEvent(handle, key, FALSE, timeout_ptr);
+    // `NtWaitForKeyedEvent` is an undocumented API where we don't known the possible
+    // return values, but they are most likely similar to `NtWaitForSingleObject`.
+    match r {
+        STATUS_SUCCESS => WakeupReason::Unknown,
+        STATUS_TIMEOUT if nt_timeout.is_some() => WakeupReason::TimedOut,
+        STATUS_ALERTED | STATUS_USER_APC => WakeupReason::Interrupt,
+        r => {
+            debug_assert!(
+                false,
+                "Unexpected return value of NtWaitForKeyedEvent: {}",
+                r
+            );
+            WakeupReason::Unknown
         }
-    } else {
-        unreachable!();
     }
 }
 
 fn release_keyed_events(key: PVOID, wake_count: usize) {
-    if let Backend::Keyed(f) = BACKEND.get() {
-        for _ in 0..wake_count {
-            (f.NtReleaseKeyedEvent)(f.handle, key, 0, ptr::null_mut());
-        }
-    } else {
-        unreachable!();
+    let handle = NT_KEYED_EVENT_HANDLE.load(Ordering::Relaxed) as HANDLE;
+    debug_assert!(handle != ptr::null());
+    let fn_ptr = NT_RELEASE_KEYED_EVENT.load(Ordering::Relaxed);
+    debug_assert!(fn_ptr != ptr::null());
+    let NtReleaseKeyedEvent: extern "system" fn(
+        EventHandle: HANDLE,
+        Key: PVOID,
+        Alertable: BOOLEAN,
+        Timeout: PLARGE_INTEGER,
+    ) -> NTSTATUS = unsafe { mem:transmute(fn_ptr) };
+
+    for _ in 0..wake_count {
+        NtReleaseKeyedEvent(handle, key, 0, ptr::null_mut());
     }
 }
 
@@ -198,103 +249,6 @@ fn convert_timeout_100ns(timeout: Option<Duration>) -> Option<LARGE_INTEGER> {
         }
         None => None,
     }
-}
-
-// Backend states
-const READY: usize = 0;
-const INITIALIZING: usize = 1;
-const EMPTY: usize = 2;
-
-pub(crate) struct BackendStatic {
-    status: AtomicUsize,
-    backend: Cell<Backend>,
-}
-pub(crate) static BACKEND: BackendStatic = BackendStatic::new();
-
-impl BackendStatic {
-    const fn new() -> Self {
-        BackendStatic {
-            status: AtomicUsize::new(EMPTY),
-            backend: Cell::new(Backend::None),
-        }
-    }
-
-    pub(crate) fn get(&self) -> Backend {
-        if self.status.load(Acquire) == READY {
-            return self.backend.get();
-        }
-        self.init()
-    }
-
-    #[inline(never)]
-    fn init(&self) -> Backend {
-        let mut status = self.status.compare_and_swap(EMPTY, INITIALIZING, Acquire);
-        if status == EMPTY {
-            let backend = if let Some(res) = ProbeWaitAddress() {
-                Backend::Wait(res)
-            } else if let Some(res) = ProbeKeyedEvent() {
-                Backend::Keyed(res)
-            } else {
-                panic!(
-                    "failed to load both NT Keyed Events (WinXP+) and \
-                     WaitOnAddress/WakeByAddress (Win8+)"
-                );
-            };
-            self.backend.set(backend);
-            self.status.store(READY, Release);
-            return backend;
-        }
-        while status != READY {
-            // The one place were we can't really do better than a spin loop is while another
-            // thread is loading the functions that provide parking primitives ;-).
-            spin_loop_hint();
-            status = self.status.load(Acquire);
-        }
-        self.backend.get()
-    }
-}
-
-unsafe impl Sync for BackendStatic {}
-
-#[derive(Clone, Copy)]
-pub(crate) enum Backend {
-    None,
-    Wait(WaitAddress),
-    Keyed(KeyedEvent),
-}
-
-// LARGE_INTEGER in WinAPI is a struct instead of integer, and not ergonomic to use.
-#[allow(non_camel_case_types)]
-type LARGE_INTEGER = i64;
-#[allow(non_camel_case_types)]
-type PLARGE_INTEGER = *mut LARGE_INTEGER;
-
-#[derive(Clone, Copy)]
-pub(crate) struct WaitAddress {
-    pub(crate) WaitOnAddress: extern "system" fn(
-        Address: PVOID,
-        CompareAddress: PVOID,
-        AddressSize: SIZE_T,
-        dwMilliseconds: DWORD,
-    ) -> BOOL,
-    pub(crate) WakeByAddressAll: extern "system" fn(Address: PVOID),
-}
-
-#[derive(Clone, Copy)]
-pub(crate) struct KeyedEvent {
-    handle: HANDLE, // The global keyed event handle.
-    NtReleaseKeyedEvent: extern "system" fn(
-        EventHandle: HANDLE,
-        Key: PVOID,
-        Alertable: BOOLEAN,
-        Timeout: PLARGE_INTEGER,
-    ) -> NTSTATUS,
-    NtWaitForKeyedEvent: extern "system" fn(
-        EventHandle: HANDLE,
-        Key: PVOID,
-        Alertable: BOOLEAN,
-        Timeout: PLARGE_INTEGER,
-    ) -> NTSTATUS,
 }
 
 #[cfg(not(feature = "fallback"))]
